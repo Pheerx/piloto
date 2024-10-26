39f9d265fcadf7210ecad134939ae609442d3f13
Mateus-carriele <160273858+Mateus-carriele@users.noreply.github.com>
2024-10-26 10:52:56 -0300

    s

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name="index"),
+    path('', views.index, name="index"),
+    path('contato/', views.index, name="entre_em_contato"),
+    path('Sobre/', views.index, name="Sobre"),
 ]
--- pweb/settings.py
+++ pweb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cyen_7czqnh3dbfj5&!4e=%#6h6y1@+ipz2guxa9km+y)rdc%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Fortaleza'
 
 USE_I18N = True
 
