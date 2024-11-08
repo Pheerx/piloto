72137406909796382b3380f34ff70520bf248b66
Mateus-carriele <160273858+Mateus-carriele@users.noreply.github.com>
2024-11-08 15:57:15 -0300

    ss

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-   
+    path('', views.index, name="index"),                         # Página inicial
+    path('exibir_item/<int:id>/', views.exibir_item, name="exibir_item"),  # Exibir item específico
+    path('dia_semana/<int:dia>/', views.dia_semana, name="dia_semana"),    # Exibir dia da semana
 ]
--- home/views.py
+++ home/views.py
@@ -1,13 +1,32 @@
 # views.py
 from django.shortcuts import render
 
+# Página inicial que usa o template base
 def index(request):
     return render(request, 'base.html')
 
+# views.py
+from django.shortcuts import render
+
+# Página que exibe um item específico
 def exibir_item(request, id):
     return render(request, 'id.html', {
-        'section': 'id',
-        'section_title': 'id',
-        'id': id
+        'id': id  # Passa o ID como variável para o template
     })
 
+# Página que exibe o dia da semana
+def dia_semana(request, dia):
+    dias_semana = {
+        1: "Domingo",
+        2: "Segunda-feira",
+        3: "Terça-feira",
+        4: "Quarta-feira",
+        5: "Quinta-feira",
+        6: "Sexta-feira",
+        7: "Sábado"
+    }
+    dia_nome = dias_semana.get(dia, "Dia inválido")
+    return render(request, 'dia_semana.html', {
+        'dia': dia,          # Passa o número do dia
+        'dia_nome': dia_nome # Passa o nome do dia da semana
+    })
