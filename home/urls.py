309a6d55dc232eb4ab99b17bd8570b6cce208236
Mateus-carriele <160273858+Mateus-carriele@users.noreply.github.com>
2024-11-07 21:23:04 -0300

    l

--- home/urls.py
+++ home/urls.py
@@ -4,9 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('home/', views.home, name="home"),
-    path('sobre/', views.sobre, name="sobre"),
-    path('contato/', views.contato, name="entre_em_contato"),
-     path('exibir_item/<int:id>/', views.exibir_item, name="exibir_item"),
-    path('diasemana/<int:dia>/', views.dia_semana, name="dia_semana"),
+   
 ]
--- home/views.py
+++ home/views.py
@@ -2,38 +2,12 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
-
-def home(request):
-    return render(request, 'home.html')
-
-def sobre(request):
-    return render(request, 'sobre.html')
-
-def contato(request):
-    return render(request, 'contato.html')
+    return render(request, 'base.html')
 
 def exibir_item(request, id):
-    return render(request, 'dashboard.html', {
-        'section': 'exibir_item',
-        'section_title': 'Exibir Item',
+    return render(request, 'id.html', {
+        'section': 'id',
+        'section_title': 'id',
         'id': id
     })
 
-def dia_semana(request, dia):
-    dias_semana = {
-        1: "Domingo",
-        2: "Segunda-feira",
-        3: "Terça-feira",
-        4: "Quarta-feira",
-        5: "Quinta-feira",
-        6: "Sexta-feira",
-        7: "Sábado"
-    }
-    dia_nome = dias_semana.get(dia, "Dia inválido")
-    return render(request, 'dashboard.html', {
-        'section': 'dia_semana',
-        'section_title': 'Dia da Semana',
-        'dia': dia,
-        'dia_nome': dia_nome
-    })
