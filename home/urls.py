010f0010f00cb3fe547eb8d34d9219303a40bd57
Pheerx <pedrohla19@gmail.com>
2024-11-10 22:30:21 -0300

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,9 @@
-# urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),                         # Página inicial
-    path('exibir_item/<int:id>/', views.exibir_item, name="exibir_item"),  # Exibir item específico
-    path('dia_semana/<int:dia>/', views.dia_semana, name="dia_semana"),    # Exibir dia da semana
+    path('', views.index, name="index"),
+    path('exibir_item/<int:id>/', views.exibir_item, name="exibir_item"),
+    path('dia_semana/<int:dia>/', views.dia_semana, name="dia_semana"),
 ]
