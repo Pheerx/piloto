e789dbd1ec5b4fec69cf32fed22f2f7f05164ebe
Pheerx <pedrohla19@gmail.com>
2024-11-21 23:59:11 -0300

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Página inicial
+    path('', views.index, name='index'),  # Página inicial
     path('exibir/<int:id>/', views.exibir_item, name='exibir_item'),
     path('dia/<int:dia>/', views.dia_semana, name='dia_semana'),
     path('contato/', views.contato, name='contato'),
